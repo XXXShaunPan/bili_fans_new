1ff5171351a22211375326230ad8ae9d91ac8001
XXXShaunPan <80847088+XXXShaunPan@users.noreply.github.com>
2022-02-21 13:38:32 +0800

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -5,9 +5,9 @@ def run(title='通知',content='内容'):
         payload = json.dumps({"ref": "main","inputs":{
             'subject':title,"content":content,"user":"630077372@qq.com"
             }})
-        header = {'Authorization': 'token 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',
+        header = {'Authorization': 'token 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',
                   "Accept": "application/vnd.github.v3+json"}
         response_decoded_json = requests.post(
-            f'https://api.github.com/repos/XXXShaunPan/bili_fans_new/actions/workflows/email.yml/dispatches?access_token=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',
+            f'https://api.github.com/repos/XXXShaunPan/bili_fans_new/actions/workflows/email.yml/dispatches?access_token=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',
             data=payload, headers=header)
         print(response_decoded_json.text)
