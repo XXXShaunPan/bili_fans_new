839b13eb82759da69860a977e57d3a32ddbd6422
XXXShaunPan <80847088+XXXShaunPan@users.noreply.github.com>
2022-02-21 15:19:29 +0800

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -1,13 +1,15 @@
 import requests
-import json
+import json, os
+
+token=os.environ["GH_TOKEN"]
 
 def run(title='通知',content='内容'):
         payload = json.dumps({"ref": "main","inputs":{
             'subject':title,"content":content,"user":"630077372@qq.com"
             }})
-        header = {'Authorization': 'token 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',
+        header = {'Authorization': f'token {token}',
                   "Accept": "application/vnd.github.v3+json"}
         response_decoded_json = requests.post(
-            f'https://api.github.com/repos/XXXShaunPan/bili_fans_new/actions/workflows/email.yml/dispatches?access_token=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',
+            f'https://api.github.com/repos/XXXShaunPan/bili_fans_new/actions/workflows/email.yml/dispatches?access_token={token}',
             data=payload, headers=header)
         print(response_decoded_json.text)
