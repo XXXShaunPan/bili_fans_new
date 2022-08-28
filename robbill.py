c3c687c4f47395ef240efea1dada35c5d5c7a1a5
XXXShaunPan <80847088+XXXShaunPan@users.noreply.github.com>
2022-08-28 17:32:29 +0800

    Update robbill.py

--- robbill.py
+++ robbill.py
@@ -81,6 +81,7 @@ print(time.time())
 if nexttime-time.time()>5:
 	print('沉睡',nexttime-time.time()-5)
 	time.sleep(nexttime-int(time.time()-5)
+
 data['token']=rob_login.login(login_acc)
 while 1:
 	res=requests.post(apiUrl,headers=header,json=data).json()
