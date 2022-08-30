bd75a1820b79607f9b886a564263a27920be5b15
XXXShaunPan <80847088+XXXShaunPan@users.noreply.github.com>
2022-08-30 10:46:28 +0800

    Update robbill.py

--- robbill.py
+++ robbill.py
@@ -78,9 +78,9 @@ nexttime=res['data']['nexttime']
 link=res['data']['pai_linkurl']
 print('next:',time.strftime("%m-%d:%H:%M:%S",time.localtime(nexttime)))
 print(time.time())
-if nexttime-time.time()>60:
-    print('沉睡',nexttime-time.time()-60)
-    time.sleep(nexttime-int(time.time()-60))
+if nexttime-time.time()>100:
+    print('沉睡',nexttime-time.time()-100)
+    time.sleep(nexttime-int(time.time()-100))
 data['token']=rob_login.login(login_acc)
 while 1:
     res=requests.post(apiUrl,headers=header,json=data).json()
